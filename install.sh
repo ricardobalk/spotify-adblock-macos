fae1ef20ce4af1ae2210a48d089a847a70b83e2d
Andrea Riolo <andry06082002@gmail.com>
2021-12-14 21:55:01 +0100

    signature fix

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cert="$*"
 	exit 1
 }
 gcc -dynamiclib -o "Chromium Embedded Framework" spotiblock.c &&
-codesign --force --deep --sign "$cert" "$path" &&
 codesign --sign "$cert" "Chromium Embedded Framework" &&
 mv -v "$libpath/Chromium Embedded Framework"{,.orig} &&
-mv -v "Chromium Embedded Framework" "$libpath"
+mv -v "Chromium Embedded Framework" "$libpath" &&
+codesign --force --deep --sign "$cert" "$path"
